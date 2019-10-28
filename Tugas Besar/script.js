143f7ad72369f7d059345f49b77621306fd46312
JodiTan <jodi.tanato@gmail.com>
2019-10-28 09:00:48 +0700

    accodtions
    accordion uda bisa

--- Tugas Besar/script.js
+++ Tugas Besar/script.js
@@ -14,6 +14,23 @@
     obj.className += " w3-flat-peter-river";
 
 
+    function openContent(obj, idContentContainer){
+    var i, x, tablinks;
+    
+    x= document.getElementsByClassName("tabs");
+    for (i=0;i<x.length;i++){
+        x[i].style.display="none";
+    }
+    
+    tablinks= document.getElementsByClassName("tablink");
+    for (i=0;i<tablinks.length;i++){
+        tablinks[i].className = tablinks[i].className.replace(" w3-flat-peter-river","");
+    }
+    
+    document.getElementById(idContentContainer).style.display = "block";
+    obj.className += " w3-flat-peter-river";
+}
+
     function showCont(id) {
         var x = document.getElementById(id);
 
