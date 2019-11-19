565a82aa8c6e40bf8860234db4809936d899aa08
Indra Permana Sugianto <49990236+inpers273@users.noreply.github.com>
2019-11-19 07:41:39 +0700

    [UPDATE] Styling, Carousel, Page No Sign Up
    Autocomplete di halaman download belum jalan, progress bar download belum jalan, carousel masih kacau.

--- Tugas Besar/script.js
+++ Tugas Besar/script.js
@@ -1,3 +1,18 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 8000); // Change image every 2 seconds
+}
+
 function openContent(obj, idContentContainer) {
     var i, x, tablinks;
 
